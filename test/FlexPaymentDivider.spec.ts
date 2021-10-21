6ba52106bf7cc1b56a799bf444ad53a9e7682bed
v-stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2021-10-21 00:10:40 -0400

    feat: test safeMode

--- test/FlexPaymentDivider.spec.ts
+++ test/FlexPaymentDivider.spec.ts
@@ -180,4 +180,199 @@ describe("FlexPaymentDivider Usage", () => {
             ).to.equal(BigNumber.from(20));
         });
     });
+
+    describe("deposit (safe mode)", () => {
+        const safeMode = 1;
+        it("holds balance of Ether for each recipient based on percentage", async () => {
+            let startingBalances = await Promise.all(recipients.map((recipient) => {
+                return paymentHandler.accumulatedBalance(recipient);
+            }));
+            const value = BigNumber.from(100);
+            let tx = await contract.deposit(safeMode, {value});
+            await tx.wait();
+            expect(
+                await ethers.provider.getBalance(paymentHandler.address),
+                "Payment handler holds balance"
+            ).to.equal(value);
+            let endingBalances = await Promise.all(recipients.map((recipient) => {
+                return paymentHandler.accumulatedBalance(recipient);
+            }));
+            for (let index = 0; index < endingBalances.length; index++) {
+                expect(
+                    endingBalances[index].sub(startingBalances[index]),
+                    "Recipient receives percentage of deposit"
+                ).to.equal(percentages[index]);
+            }
+        });
+        it("assigns change to recipients when division rounds towards 0", async () => {
+            let startingBalances = await Promise.all(recipients.map((recipient) => {
+                return paymentHandler.accumulatedBalance(recipient);
+            }));
+            const value = BigNumber.from(10);
+            let tx = await contract.deposit(safeMode, {value});
+            await tx.wait();
+            expect(
+                await ethers.provider.getBalance(paymentHandler.address),
+                "Payment handler holds balance"
+            ).to.equal(value);
+            let endingBalances = await Promise.all(recipients.map((recipient) => {
+                return paymentHandler.accumulatedBalance(recipient);
+            }));
+
+            expect(
+                endingBalances[0].sub(startingBalances[0]),
+                "Aloe received 3 wei"
+            ).to.equal(percentages[0].div(10));
+            const aloeBalance = await paymentHandler.accumulatedBalance(recipients[0]);
+            const aloeChange = await paymentHandler.accumulatedChange(recipients[0]);
+            expect(
+                aloeChange,
+                "Aloe is eligible for 0/100 change"
+            ).to.equal(BigNumber.from(0));
+
+            expect(
+                endingBalances[1].sub(startingBalances[1]),
+                "Bill received 1 wei"
+            ).to.equal(percentages[1].div(10));
+            const billBalance = await paymentHandler.accumulatedBalance(recipients[1]);
+            const billChange = await paymentHandler.accumulatedChange(recipients[1]);
+            expect(
+                billChange,
+                "Bill is eligible for 0/100 change"
+            ).to.equal(BigNumber.from(0));
+
+            expect(
+                endingBalances[2].sub(startingBalances[2]),
+                "Cate has not yet received wei"
+            ).to.equal(BigNumber.from(0));
+            const cateBalance = await paymentHandler.accumulatedBalance(recipients[2]);
+            const cateChange = await paymentHandler.accumulatedChange(recipients[2]);
+            expect(
+                cateChange,
+                "Cate is eligible for 60/100 change"
+            ).to.equal(BigNumber.from(60));
+
+            expect(
+                endingBalances[3].sub(startingBalances[3]),
+                "Dane received 5 wei"
+            ).to.equal(BigNumber.from(5));
+            const daneBalance = await paymentHandler.accumulatedBalance(recipients[3]);
+            const daneChange = await paymentHandler.accumulatedChange(recipients[3]);
+            expect(
+                daneChange,
+                "Dane is eligible for 40/100 change"
+            ).to.equal(BigNumber.from(40));
+
+            expect(
+                await ethers.provider.getBalance(paymentHandler.address),
+                "paymentHandler holds balance for change"
+            ).to.equal(
+                aloeChange.add(
+                    billChange.add(cateChange.add(daneChange))
+                ).div(100).add(
+                    aloeBalance.add(billBalance.add(cateBalance.add(daneBalance)))
+                )
+            );
+        });
+        it("increases balance when enough change is accumulated", async () => {
+            const cateAddress = recipients[2];
+
+            const cateBalance1 = await ethers.provider.getBalance(cateAddress);
+            const contractBalance1 = await ethers.provider.getBalance(paymentHandler.address);
+
+            let tx = await contract.deposit(safeMode, {value: BigNumber.from(10)});
+            await tx.wait();
+
+            const cateBalance2 = await ethers.provider.getBalance(cateAddress);
+            const contractBalance2 = await ethers.provider.getBalance(paymentHandler.address);
+
+            expect(
+                await paymentHandler.accumulatedBalance(cateAddress),
+                "Cate does not accumulate a balance upon the first deposit"
+            ).to.equal(BigNumber.from(0));
+            expect(
+                await paymentHandler.accumulatedChange(cateAddress),
+                "Cate's accumulated change is set"
+            ).to.equal(percentages[2].mul(10));
+            expect(
+                contractBalance2.sub(contractBalance1),
+                "The paymentHandler stores the total value to later be divided amongst recipients"
+            ).to.equal(BigNumber.from(10));
+
+            tx = await contract.deposit(safeMode, {value: BigNumber.from(10)});
+            await tx.wait();
+
+            const cateBalance3 = await ethers.provider.getBalance(cateAddress);
+            const contractBalance3 = await ethers.provider.getBalance(paymentHandler.address);
+
+            expect(
+                await paymentHandler.accumulatedBalance(cateAddress),
+                "The change over 100 gets added to Cate's balance"
+            ).to.equal(BigNumber.from(1));
+            expect(
+                contractBalance3.sub(contractBalance2),
+                "Because Cate does not receive the payment the contract balance does not change"
+            ).to.equal(BigNumber.from(10));
+
+            expect(
+                await paymentHandler.accumulatedChange(cateAddress),
+                "Cate still has some change remaining"
+            ).to.equal(BigNumber.from(20));
+        });
+    });
+
+    describe("deposit without safe mode after safe mode deposit", () => {
+        it("sends accumulated balance and holds change", async () => {
+            const cateAddress = recipients[2];
+
+            const cateBalance1 = await ethers.provider.getBalance(cateAddress);
+            const contractBalance1 = await ethers.provider.getBalance(paymentHandler.address);
+
+            let tx = await contract.deposit(1, {value: BigNumber.from(10)});
+            await tx.wait();
+
+            const cateBalance2 = await paymentHandler.accumulatedBalance(cateAddress);
+            const contractBalance2 = await ethers.provider.getBalance(paymentHandler.address);
+
+            expect(
+                await paymentHandler.accumulatedBalance(cateAddress),
+                "Cate does not accumulate a balance upon the first deposit"
+            ).to.equal(BigNumber.from(0));
+            expect(
+                await paymentHandler.accumulatedChange(cateAddress),
+                "Cate's accumulated change is set"
+            ).to.equal(percentages[2].mul(10));
+            expect(
+                contractBalance2.sub(contractBalance1),
+                "The paymentHandler stores the total value to later be divided amongst recipients"
+            ).to.equal(BigNumber.from(10));
+
+            tx = await contract.deposit(0, {value: BigNumber.from(10)});
+            await tx.wait();
+
+            const cateBalance3 = await ethers.provider.getBalance(cateAddress);
+            const contractBalance3 = await ethers.provider.getBalance(paymentHandler.address);
+
+            let endingChange = await Promise.all(recipients.map((recipient) => {
+                return paymentHandler.accumulatedChange(recipient);
+            }));
+            let endingChangeSum = endingChange.reduce((prev, curr) => {
+                return prev.add(curr)
+            });
+
+            expect(
+                cateBalance3.sub(cateBalance1),
+                "The change over 100 and previous balance gets transferred to Cate"
+            ).to.equal(BigNumber.from(1).add(cateBalance2));
+            expect(
+                contractBalance3,
+                "Because Cate receives the payment the paymentHandler balance is the sum of the change"
+            ).to.equal(endingChangeSum.div(100));
+
+            expect(
+                await paymentHandler.accumulatedChange(cateAddress),
+                "Cate still has some change remaining"
+            ).to.equal(BigNumber.from(20));
+        });
+    });
 });
