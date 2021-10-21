cc4659a1351c5d2409adfcade863babf455be5a9
v-stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2021-10-20 22:45:42 -0400

    fix: update tests to check payment handler contract

--- test/FlexPaymentDivider.spec.ts
+++ test/FlexPaymentDivider.spec.ts
@@ -3,10 +3,11 @@ import { ethers } from "hardhat";
 import { Contract } from "ethers";
 import { BigNumber } from "@ethersproject/bignumber";
 
-describe("FlexPaymentDivider", () => {
+describe("FlexPaymentDivider Usage", () => {
     let contract: Contract;
     let recipients: Array<string>;
     let percentages: Array<BigNumber>;
+    let paymentHandler: Contract;
     
     beforeEach(async () => {
         const [deployer, aloe, bill, cate, dane] = await ethers.getSigners();
@@ -22,19 +23,55 @@ describe("FlexPaymentDivider", () => {
             BigNumber.from(6),
             BigNumber.from(54)
         ];
-        const Contract = await ethers.getContractFactory("FlexPaymentDivider");
+        const Contract = await ethers.getContractFactory(
+            "FlexPaymentDividerTest"
+        );
         contract = await Contract.deploy(
             recipients,
             percentages
         );
+        const paymentHandlerAddress = await contract.paymentHandler();
+        paymentHandler = await ethers.getContractAt(
+            "FlexPaymentDivider",
+            paymentHandlerAddress
+        );
+    });
+
+    it("paymentHandler sets recipients and percentages", async () => {
+        const recipientCount = await paymentHandler.recipientCount();
+        expect(
+            recipientCount,
+            "Recipient count matches number of addresses input upon deployment"
+        ).to.equal(recipients.length);
+
+        let recipientsById = await Promise.all(recipients.map((recipient, id) => {
+            return paymentHandler.recipientById(id);
+        }));
+        for (let index = 0; index < recipientsById.length; index++) {
+            expect(
+                recipients[index],
+                "Recipient ids correspond to order they were input upon deployment"
+            ).to.equal(recipientsById[index]);
+        }
+
+        let percentagesByRecipient = await Promise.all(recipients.map((recipient) => {
+            return paymentHandler.percentage(recipient);
+        }));
+        for (let index = 0; index < percentagesByRecipient.length; index++) {
+            expect(
+                percentages[index],
+                "Recipient percentages correspond to order they were input upon deployment"
+            ).to.equal(percentagesByRecipient[index]);
+        }
     });
 
     describe("deposit (without safe mode)", () => {
+        const safeMode = 0;
         it("sends amount of Ether to each recipient based on percentage", async () => {
             let startingBalances = await Promise.all(recipients.map((recipient) => {
                 return ethers.provider.getBalance(recipient);
             }));
-            let tx = await contract.deposit(0, {value: BigNumber.from(100)});
+            let tx = await contract.deposit(safeMode, {value: BigNumber.from(100)});
             await tx.wait();
             let endingBalances = await Promise.all(recipients.map((recipient) => {
                 return ethers.provider.getBalance(recipient);
@@ -50,7 +87,7 @@ describe("FlexPaymentDivider", () => {
             let startingBalances = await Promise.all(recipients.map((recipient) => {
                 return ethers.provider.getBalance(recipient);
             }));
-            let tx = await contract.deposit({value: BigNumber.from(10)});
+            let tx = await contract.deposit(safeMode, {value: BigNumber.from(10)});
             await tx.wait();
             let endingBalances = await Promise.all(recipients.map((recipient) => {
                 return ethers.provider.getBalance(recipient);
@@ -60,7 +97,7 @@ describe("FlexPaymentDivider", () => {
                 endingBalances[0].sub(startingBalances[0]),
                 "Aloe received 3 wei"
             ).to.equal(percentages[0].div(10));
-            const aloeChange = await contract.accumulatedChange(recipients[0]);
+            const aloeChange = await paymentHandler.accumulatedChange(recipients[0]);
             expect(
                 aloeChange,
                 "Aloe is eligible for 0/100 change"
@@ -70,7 +107,7 @@ describe("FlexPaymentDivider", () => {
                 endingBalances[1].sub(startingBalances[1]),
                 "Bill received 1 wei"
             ).to.equal(percentages[1].div(10));
-            const billChange = await contract.accumulatedChange(recipients[1]);
+            const billChange = await paymentHandler.accumulatedChange(recipients[1]);
             expect(
                 billChange,
                 "Bill is eligible for 0/100 change"
@@ -80,7 +117,7 @@ describe("FlexPaymentDivider", () => {
                 endingBalances[2].sub(startingBalances[2]),
                 "Cate has not yet received wei"
             ).to.equal(BigNumber.from(0));
-            const cateChange = await contract.accumulatedChange(recipients[2]);
+            const cateChange = await paymentHandler.accumulatedChange(recipients[2]);
             expect(
                 cateChange,
                 "Cate is eligible for 60/100 change"
@@ -90,28 +127,28 @@ describe("FlexPaymentDivider", () => {
                 endingBalances[3].sub(startingBalances[3]),
                 "Dane received 5 wei"
             ).to.equal(BigNumber.from(5));
-            const daneChange = await contract.accumulatedChange(recipients[3]);
+            const daneChange = await paymentHandler.accumulatedChange(recipients[3]);
             expect(
                 daneChange,
                 "Dane is eligible for 40/100 change"
             ).to.equal(BigNumber.from(40));
 
             expect(
-                await ethers.provider.getBalance(contract.address),
-                "Contract holds balance for change"
+                await ethers.provider.getBalance(paymentHandler.address),
+                "paymentHandler holds balance for change"
             ).to.equal(aloeChange.add(billChange.add(cateChange.add(daneChange))).div(100));
         });
         it("sends amount of Ether when enough change is accumulated", async () => {
             const cateAddress = recipients[2];
 
             const cateBalance1 = await ethers.provider.getBalance(cateAddress);
-            const contractBalance1 = await ethers.provider.getBalance(contract.address);
+            const contractBalance1 = await ethers.provider.getBalance(paymentHandler.address);
 
-            let tx = await contract.deposit({value: BigNumber.from(10)});
+            let tx = await contract.deposit(safeMode, {value: BigNumber.from(10)});
             await tx.wait();
 
             const cateBalance2 = await ethers.provider.getBalance(cateAddress);
-            const contractBalance2 = await ethers.provider.getBalance(contract.address);
+            const contractBalance2 = await ethers.provider.getBalance(paymentHandler.address);
 
             expect(
                 cateBalance2.sub(cateBalance1),
@@ -119,14 +156,14 @@ describe("FlexPaymentDivider", () => {
             ).to.equal(BigNumber.from(0));
             expect(
                 contractBalance2.sub(contractBalance1),
-                "The contract stores the total change to later be divided amongst recipients"
+                "The paymentHandler stores the total change to later be divided amongst recipients"
             ).to.equal(BigNumber.from(1));
 
-            tx = await contract.deposit({value: BigNumber.from(10)});
+            tx = await contract.deposit(safeMode, {value: BigNumber.from(10)});
             await tx.wait();
 
             const cateBalance3 = await ethers.provider.getBalance(cateAddress);
-            const contractBalance3 = await ethers.provider.getBalance(contract.address);
+            const contractBalance3 = await ethers.provider.getBalance(paymentHandler.address);
 
             expect(
                 cateBalance3.sub(cateBalance2),
@@ -138,7 +175,7 @@ describe("FlexPaymentDivider", () => {
             ).to.equal(BigNumber.from(0));
 
             expect(
-                await contract.accumulatedChange(cateAddress),
+                await paymentHandler.accumulatedChange(cateAddress),
                 "Cate still has some change remaining"
             ).to.equal(BigNumber.from(20));
         });
